a12dee297814438d4673ee1fcc85f793a9084a19
Sean O'Loughlin <CeleryMan@Seans-MacBook-Pro.local>
2012-10-05 00:46:05 -0500

    pretty much completed aside from some extra considerations

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,10 +9,25 @@ class HomeController < ApplicationController
   def crawl
   	@site = params[:site]
   	
+  	#get all non-html text on page as a string, then split it up
   	doc = Nokogiri::HTML(open(@site))
-	@output = doc.xpath("//text()").to_s
-  	
-  	
+  	doc.css('script').remove
+  	doc.xpath("//@*[starts-with(name(), 'on')]").remove
+	urlString = doc.xpath("//text()").to_s
+	urlString = urlString.gsub(/[^A-Za-z].-/, ' ')
+  	tokenized = urlString.split(" ")
+
+  	#create or increment each word
+  	tokenized.each do |token|
+  		token = token.downcase
+  		word = Word.find_by_name(token) || Word.new(:name => token)
+  		word.increment!(:count)
+  		word.save
+  	end
+
+  	# get top 10 words by count
+  	@words = Word.all(:limit => 10, :order => "count desc")
+
 	render :action => "display"
   end
 
